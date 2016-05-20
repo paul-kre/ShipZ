c09d6335d74f26acc1cc22646568b7a5053e20f0
Paul Kretschel <paul.kretschel@icloud.com>
2016-05-20 11:21:26 +0200

    Update event id's

--- src/shipz/util/EventIds.java
+++ src/shipz/util/EventIds.java
@@ -4,8 +4,13 @@ package shipz.util;
  * Created by paulkretschel on 17/05/16.
  */
 public interface EventIds {
+
+    // General
     static final byte SHOOT_EVENT = 10;
-    static final byte SHOOT_RESULT = 11;
     static final byte CLOSE_EVENT = 20;
+
+    // Network
     static final byte DISCONNECT_EVENT = 30;
+    static final byte SHOOT_RESULT = 31;
+
 }
