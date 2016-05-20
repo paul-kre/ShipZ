1582b39c0f792dc50e0f589e823f6d4d0cdfa9aa
Florian Osterberg <florian.osterberg@web.de>
2016-05-20 11:51:02 +0200

    IO-Events hinzugefügt

--- src/shipz/util/EventIds.java
+++ src/shipz/util/EventIds.java
@@ -12,5 +12,11 @@ public interface EventIds {
     // Network
     static final byte DISCONNECT_EVENT = 30;
     static final byte SHOOT_RESULT = 31;
+    
+    // IO
+    static final byte GAME_CREATED = 110;
+    static final byte GAME_SAVED = 111;
+    static final byte GAME_LOADED = 112;
+    // IDs von 110-120 reserviert
 
 }
