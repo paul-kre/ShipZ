8ead21969996fdabb43834265dc89c923f2bfad4
Paul Kretschel <paul.kretschel@icloud.com>
2016-05-24 10:24:53 +0200

    Erase statics

--- src/shipz/util/EventIds.java
+++ src/shipz/util/EventIds.java
@@ -6,19 +6,19 @@ package shipz.util;
 public interface EventIds {
 
     // General
-    static final byte SHOOT_EVENT = 10;
-    static final byte CLOSE_EVENT = 20;
+    final byte SHOOT_EVENT = 10;
+    final byte CLOSE_EVENT = 20;
 
     // Network
-    static final byte DISCONNECT_EVENT = 30;
-    static final byte SHOOT_RESULT = 31;
+    final byte DISCONNECT_EVENT = 30;
+    final byte SHOOT_RESULT = 31;
     
     // IO
-    static final byte SAVE_EVENT = 110;
-    static final byte LOAD_EVENT = 111;
-    static final byte UNDO_EVENT = 112;
-    static final byte REDO_EVENT = 113;
-    static final byte HIGHSCORE_EVENT = 114;
+    final byte SAVE_EVENT = 110;
+    final byte LOAD_EVENT = 111;
+    final byte UNDO_EVENT = 112;
+    final byte REDO_EVENT = 113;
+    final byte HIGHSCORE_EVENT = 114;
     
     // IDs von 110-120 reserviert
     
