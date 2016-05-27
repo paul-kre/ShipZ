03c868d28bce2db0bfde1138c931d24c6dfe0ea2
Florian Osterberg <florian.osterberg@web.de>
2016-05-27 16:09:11 +0200

    Enum Event hinzugefügt
    Sinnvollere Art, die einzelnen Event-IDs anzulegen.

---
+++ src/shipz/util/Event.java
@@ -0,0 +1,34 @@
+package shipz.util;
+
+/**
+ * Die einzelnen Event-IDs werden als Aufzählungstyp abgespeichert.
+ * Zugriff auf die einzelnen Werte:
+ * Event.SHOOT_EVENT.value
+ * @author Florian Osterberg
+ */
+public enum Event {
+
+	// General
+	SHOOT_EVENT(10),
+	CLOSE_EVENT(20),
+	
+	// Network
+	DISCONNECT_EVENT(30),
+	SHOOT_RESULT(31),
+	
+	// IO
+	SAVE_EVENT(110),
+	LOAD_EVENT(111),
+	UNDO_EVENT(112),
+	REDO_EVENT(113),
+	HIGHSCORE_EVENT(114);
+	
+	// IV
+	private int value;
+	
+	// Constructor
+	private Event(int value) {
+		this.value = value;
+	}
+	
+}
