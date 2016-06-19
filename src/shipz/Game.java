1e87649755d0d1a1819cf77670d390917a688ef5
Paul Kretschel <paul.kretschel@icloud.com>
2016-06-19 16:27:48 +0200

    Change "EventIds" to "Event"

--- src/shipz/Game.java
+++ src/shipz/Game.java
@@ -9,9 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import shipz.gamemode.*;
 
-import static shipz.util.EventIds.FINISHED_ROUND;
-import static shipz.util.EventIds.READY_EVENT;
-
 /**
  * Spielverwaltung
  * @author Max
